9a493b75ddb556278f4ff3ce522a18e317540ee2
Khushi Kumari <khushiiob2001@gmail.com>
2023-12-10 19:37:38 +0530

    new commit

--- src/main/java/com/spring/Teacher.java
+++ src/main/java/com/spring/Teacher.java
@@ -1,6 +1,15 @@
 package com.spring;
 
 public class Teacher implements School {
+    public String course;
+    public void setCourse(String course){
+        this.course = course;
+    }
+    public String getCourse(){
+        return course;
+    }
+
+
     public void detail(){
         System.out.println("Teacher detail");
     }
--- src/main/java/com/spring/main.java
+++ src/main/java/com/spring/main.java
@@ -7,7 +7,8 @@ public class main {
     public static void main(String args[]){
        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
       // Teacher student = context.getBean(Teacher.class);//or
-        School school = (Teacher) context.getBean("teacher");//using the id of the bean we can create the object but here we have to do typecasting (Teacher) beacuse it will receive the type as object
+        Teacher school = (Teacher) context.getBean("teacher");//using the id of the bean we can create the object but here we have to do typecasting (Teacher) beacuse it will receive the type as object
        school.detail();
+       System.out.println(school.getCourse());
     }
 }
